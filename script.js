8147011c664edf0a375539eea333ba19348aa1b7
Blaq2c <t.mwangim02@gmail.com>
2024-03-27 04:06:06 +0300

    analog.clock

---
+++ script.js
@@ -0,0 +1,20 @@
+function updateClock() {
+    const now = new Date();
+    const hour = now.getHours();
+    const minute = now.getMinutes();
+    const second = now.getSeconds();
+
+    const hourHand = document.getElementById('hourHand');
+    const minuteHand = document.getElementById('minuteHand');
+    const secondHand = document.getElementById('secondHand');
+
+    const hourDegrees = (hour * 30) + (minute * 0.5); 
+    const minuteDegrees = (minute * 6) + (second * 0.1); 
+    const secondDegrees = second * 6; 
+
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+    minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
+    secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+}
+
+setInterval(updateClock, 1000); 
